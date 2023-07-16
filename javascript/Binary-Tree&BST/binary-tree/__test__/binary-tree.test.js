a85906297b7504e1c99e09c9910b2fb88c8daa6f
AmroBaniIssa <amro.nedal.issa@gmail.com>
2023-07-16 04:21:09 +0000

    .

---
+++ javascript/Binary-Tree&BST/binary-tree/__test__/binary-tree.test.js
@@ -0,0 +1,48 @@
+'use strict';
+const BinaryTree = require('../binary-tree');
+const Node = require('../Node');
+
+let tree = null;
+
+describe("Binary Tree", () => {
+    beforeAll(() => {
+        
+let one = new Node(1);
+let two = new Node(2);
+let three = new Node(3);
+let four = new Node(4);
+let five = new Node(5);
+let six = new Node(6);
+let seven = new Node(7);
+
+one.left = two;
+one.right = three;
+two.left = four;
+two.right =five;
+three.left = six;
+three.right = seven;
+
+
+tree = new BinaryTree(one);
+    });
+    it("constructor", () => {
+        const newTree = new BinaryTree();
+        expect(newTree.root).toBeNull();
+        expect(tree.root.value).toEqual(1);
+    });
+    it('preOrder', () => {
+        let expectedOutput = [ 1, 2, 4, 5,3, 6, 7];
+        let preOrder = tree.preOrder();
+        expect(preOrder).toEqual(expectedOutput);
+    });
+    it('inOrder', () => {
+        let expectedOutput = [4, 2, 5, 1,6, 3, 7];
+        let inOrder = tree.inOrder();
+        expect(inOrder).toEqual(expectedOutput);
+    });
+    it('postOrder', () => {
+        let expectedOutput = [ 4, 5, 2, 6,7, 3, 1];
+        let postOrder = tree.postOrder();
+        expect(postOrder).toEqual(expectedOutput);
+    });
+})
---
+++ javascript/Binary-Tree&BST/binary-tree/index.js
@@ -0,0 +1,31 @@
+const BinaryTree=require('./binary-tree')
+const Node=require('./Node')
+
+
+
+let one = new Node(1);
+let two = new Node(2);
+let three = new Node(3);
+let four = new Node(4);
+let five = new Node(5);
+let six = new Node(6);
+let seven = new Node(7);
+
+one.left = two;
+one.right = three;
+two.left = four;
+two.right =five;
+three.left = six;
+three.right = seven;
+
+
+tree = new BinaryTree(one);
+
+let preOrder = tree.preOrder();
+let inOrder = tree.inOrder();
+let postOrder = tree.postOrder();
+
+
+console.log('preOrder: ', preOrder);
+console.log('inOrder: ', inOrder);
+console.log('postOrder: ', postOrder);
