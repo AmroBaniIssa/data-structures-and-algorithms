bdd7d72e97922fadaa92c85321bacd363aa21d1a
AmroBaniIssa <amro.nedal.issa@gmail.com>
2023-08-29 06:47:45 +0000

    .

--- javascript/hashtable/hashtable.test.js
+++ javascript/hashtable/hashtable.test.js
@@ -1,51 +1,43 @@
-const Hashmap = require("./hashtable"); 
-const assert = require("assert");
+const Hashmap = require("./hashtable");
 
 describe("Hashmap Functionality", () => {
-    let myHashmap;
+  let myHashmap;
 
-    beforeEach(() => {
-        myHashmap = new Hashmap(8);
-    });
+  beforeEach(() => {
+    myHashmap = new Hashmap(20);
+  });
 
-    it("should set a key and retrieve the value", () => {
-        myHashmap.add("apple", "red");
-        assert.strictEqual(myHashmap.get("apple"), "red");
-    });
+  it("should set a key and retrieve the value", () => {
+    myHashmap.add("ahmad", "owner");
+    expect(myHashmap.get("ahmad")).toBe("owner");
+  });
 
-    it("should return null if the keydoesn't exist", () => {
-        assert.strictEqual(myHashmap.get("banana"), null);
-    });
+  it("should return null if the key doesn't exist", () => {
+    expect(myHashmap.get("sameeer")).toBeNull();
+  });
 
-    it("should return a list of all unique keys", () => {
-        myHashmap.add("apple", "red");
-        myHashmap.add("banana", "yellow");
-        myHashmap.add("cherry", "red");
+  it("should return a list of all keys", () => {
+    myHashmap.add("ahmad", "user");
+    myHashmap.add("khaled", "owner");
+    myHashmap.add("amro", "user");
 
-        const keys = myHashmap.keys();
-        assert.deepStrictEqual(keys, ["apple", "banana", "cherry"]);
-    });
+    const keys = myHashmap.keys();
+    expect(keys).toEqual(expect.arrayContaining(["ahmad", "khaled", "amro"]));
+  });
 
-    it("should handle collisions", () => {
-        // Assuming your hash function generates collisions for "apple" and "banana"
-        myHashmap.add("apple", "fruit");
-        myHashmap.add("banana", "fruit");
+  it("should handle collisions", () => {
+    myHashmap.add("ahmad", "user");
+    myHashmap.add("hamad", "user");
 
-        assert.strictEqual(myHashmap.get("apple"), "fruit");
-        assert.strictEqual(myHashmap.get("banana"), "fruit");
-    });
+    expect(myHashmap.get("ahmad")).toBe("user");
+    expect(myHashmap.get("hamad")).toBe("user");
+  });
 
-    it("should retrieve a value from a collision bucket", () => {
-        // Assuming your hash function generates a collision for "apple" and "banana"
-        myHashmap.add("apple", "fruit");
-        myHashmap.add("banana", "fruit");
+  
 
-        // This will retrieve from the same bucket
-        assert.strictEqual(myHashmap.get("banana"), "fruit");
-    });
-
-    it("should hash a key to an in-range value", () => {
-        const hashValue = myHashmap.hash("apple");
-        assert.ok(hashValue >= 0 && hashValue < myHashmap.size);
-    });
+  it("should hash a key for the value", () => {
+    const hashValue = myHashmap.hash("ahmad");
+    expect(hashValue).toBeGreaterThanOrEqual(0);
+    expect(hashValue).toBeLessThan(myHashmap.size);
+  });
 });
